d404776fc88a192ec07b0a6ac4c95462746a5809
odf <olaf.delgado@gmail.com>
2014-10-24 12:30:06 +1100

    extract deferred matchers to a helpers file

--- __tests__/deferred_spec.js
+++ __tests__/deferred_spec.js
@@ -1,47 +1,11 @@
 'use strict';
 
 
+jest.dontMock('./helpers');
 jest.dontMock('../dist/index');
 
+require('./helpers');
 var csp = require('../dist/index');
-
-
-var customMatchers = {
-  toResolve: function() {
-    var value;
-
-    this.actual.then(function(val) { value = val; },
-                     function() {});
-
-    return value !== undefined;
-  },
-  toResolveAs: function(expected) {
-    var value;
-
-    this.actual.then(function(val) { value = val; },
-                     function() {});
-
-    return value === expected;
-  },
-  toBeRejected: function() {
-    var value;
-
-    this.actual.then(function() {},
-                     function(msg) { value = msg; });
-
-    return value !== undefined;
-  },
-  toBeRejectedWith: function(expected) {
-    var value;
-
-    this.actual.then(function() {},
-                     function(msg) { value = msg; });
-
-    return value === expected;
-  }
-};
-
-beforeEach(function() { this.addMatchers(customMatchers); });
 
 
 describe('a deferred', function() {
