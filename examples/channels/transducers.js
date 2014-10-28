ba1fd417633d5f6f4c99553d709d2f4326da0860
odf <olaf.delgado@gmail.com>
2014-10-28 13:21:13 +1100

    in transducer example, add a fixed version of mapcat

--- examples/channels/transducers.js
+++ examples/channels/transducers.js
@@ -3,12 +3,51 @@
 var t = require('transducers.js');
 
 
+function Cat(xform) {
+  this.xform = xform;
+}
+
+Cat.prototype.init = function() {
+  return this.xform.init();
+};
+
+Cat.prototype.result = function(v) {
+  return this.xform.result(v);
+};
+
+Cat.prototype.step = function(result, input) {
+  var xform = this.xform;
+  var newxform = {
+    init: function() {
+      return xform.init();
+    },
+    result: function(v) {
+      return v;
+    },
+    step: function(result, input) {
+      var val = xform.step(result, input);
+      return (val && val.__transducers_reduced__) ? new t.Reduced(val) : val;
+    }
+  }
+
+  return t.reduce(input, newxform, result);
+};
+
+function cat(xform) {
+  return new Cat(xform);
+}
+
+function mapcat(f, ctx) {
+  return t.compose(t.map(f), cat);
+}
+
+
 var xf = t.compose(
-  t.mapcat(function(x) { return [x, x*x]; }),
-  t.take(14)//,
-  //t.partitionBy(function(x) { return x % 3; }),
-  //t.filter(function(x) { return x.length > 1; }),
-  //t.map(function(x) { return x.join('#'); })
+  mapcat(function(x) { return [x, x*x]; }),
+  t.take(14),
+  t.partitionBy(function(x) { return x % 3; }),
+  t.filter(function(x) { return x.length > 1; }),
+  t.map(function(x) { return x.join('#'); })
 );
 
 var reductions = function(fn, start) {
@@ -49,10 +88,8 @@ var tchan = function(xf, start, stop) {
 
   async(function*() {
     var i, r;
-    for (i = (start || 0), r = true; r && i != (stop || 10); ++i) {
+    for (i = (start || 0), r = true; r && i != stop; ++i)
       r = yield ch.push(i);
-      console.log('push '+i+' => '+r);
-    }
     ch.close();
   });
 
@@ -63,7 +100,7 @@ var tchan = function(xf, start, stop) {
 async(function*() {
   console.log('expected: '+JSON.stringify(t.seq([0,1,2,3,4,5,6,7,8,9], xf))); 
 
-  yield csp.each(console.log, tchan(xf));
+  yield csp.each(console.log, tchan(xf, 1));
 
   console.log([1,2,3,4,5]);
   console.log(t.seq([1,2,3,4,5], sums));
